cb434ef4713be34a22315792d6371d3c231db268
Celal Cankaya <celalcankaya@outlook.com>
2019-08-02 15:38:25 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+  attr_accessor :name, :genre, :artist
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    genre.add_song(self)
+    @@all << self
+  end
+end
