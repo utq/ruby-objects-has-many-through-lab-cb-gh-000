1e5d615e16b13deb7de82f2da56921dff50d68c2
Celal Cankaya <celalcankaya@outlook.com>
2019-08-02 15:33:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,24 @@
 class Artist
-  attr_accessor :name
-
-   @@song_count = 0
+  attr_accessor :name, :songs
 
    def initialize(name)
-    @name = name
-    @songs = []
+      @name = name
+      @songs = []
   end
 
-   def self.song_count
-    @@song_count
+   def add_song(song)
+      @songs << song
+      song.artist = self
   end
 
    def songs
-    @songs
+      @songs
   end
 
-   def add_song(song_name)
-    @songs << song_name
-    song_name.artist = self
-    @@song_count += 1
+   def genres
+    self.songs.collect do |song|
+      song.genre
+    end
   end
 
-   def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-end 
+ end
