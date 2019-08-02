befc5869c9077bc39b27b77ab2ef4de2812abaf9
Celal Cankaya <celalcankaya@outlook.com>
2019-08-02 15:33:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,10 @@
-class Author
-  attr_accessor :name
+class Song
+  attr_accessor :name, :artist, :genre
 
-   @@post_count = 0
-
-   def initialize(name)
-    @name = name
-    @posts = []
+   def initialize(name, genre)
+      @name = name
+      @genre = genre
+      genre.add_song(self)
   end
 
-   def posts
-    @posts
-  end
-
-   def self.post_count
-    @@post_count
-  end
-
-   def add_post(post_title)
-    @posts << post_title
-    post_title.author = self
-    @@post_count += 1
-  end
-
-   def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-end 
+ end
